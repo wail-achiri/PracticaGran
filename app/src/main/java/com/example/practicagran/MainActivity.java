a52d7785a569de936f8ab268069a11d760889f37
1 <mzafram2001@gmail.com>
2020-11-09 18:33:13 +0100

    - Tags de restaurant y parking añadidos. - Implementado en el MainActivity.java el OnClickListener. - Linkear la actividad de parkings con el principal.

--- app/src/main/java/com/example/practicagran/MainActivity.java
+++ app/src/main/java/com/example/practicagran/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.practicagran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ImageView restaurants;
+    ImageView parkings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        restaurants = findViewById(R.id.restaurant);
+        restaurants.setOnClickListener(this);
+
+        parkings = findViewById(R.id.parking);
+        parkings.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        ImageView imatgeCategoria = (ImageView) v;
+        String nomImatge = String.valueOf(imatgeCategoria.getTag());
+
+
+        if(nomImatge.equals("parking"))
+        {
+            Intent intent = new Intent(this,Parkings.class);
+            startActivity(intent);
+        }
+        else if(nomImatge.equals("restaurant"))
+        {
+            ///Intent intent = new Intent(this,Restaurants.class);
+            ///startActivity(intent);
+        }
     }
 }
