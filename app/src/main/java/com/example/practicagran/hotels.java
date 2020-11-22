6d6ed2dff39611fb51df35e6c78b6440c3a42773
wail-achiri <wail.elachiri@gmail.com>
2020-11-22 16:56:59 +0100

    -He arreglado lo del rating bar ya que me daba errores en todo -He cambiado el nombre de extraicovalora -En el código restaurants he cambiado la declaración de variables pq era mucho -El diseño de hotels esta casi acabado faltan pequeños detalles

--- app/src/main/java/com/example/practicagran/hotels.java
+++ app/src/main/java/com/example/practicagran/hotels.java
@@ -14,14 +14,10 @@ import android.widget.TextView;
 public class hotels extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     String[] urls = {"https://www.hoteliris.com","https://www.hotelaugustavalles.com","https://www.hotelciutatgranollers.com","https://www.hotelgranollers.com"};
     String[] contacte = {"938792929","938456050","938796220","938795100"};
-    TextView webIris;
-    TextView webAugusta;
-    TextView webCiutat;
-    TextView webGranollers;
-    TextView trucaIris;
-    TextView trucaAugusta;
-    TextView trucaCiutat;
-    TextView trucaGranollers;
+    String[] mapaHotel = {"41.60165735046227,2.2902210422999985","41.56634770229584, 2.2824184340010687","41.600971486049424, 2.2960760903806525", "41.5945960158185, 2.2855305304855325"};
+    TextView webIris,webAugusta,webCiutat,webGranollers;
+    TextView trucaIris,trucaAugusta,trucaCiutat,trucaGranollers;
+    TextView mapaIris,mapaAugusta,mapaCiutat,mapaGranollers;
     CardView card1,card2,card3,card4;
     Spinner hotel_spinner;
 
@@ -31,6 +27,7 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hotels);
 
+        //WEBS
         webIris = findViewById(R.id.webIris);
         webIris.setOnClickListener(this);
 
@@ -43,6 +40,7 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
         webGranollers = findViewById(R.id.webGrano);
         webGranollers.setOnClickListener(this);
 
+        //TRUCAR
         trucaIris = findViewById(R.id.trucaIris);
         trucaIris.setOnClickListener(this);
 
@@ -54,6 +52,21 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
 
         trucaGranollers = findViewById(R.id.trucaGrano);
         trucaGranollers.setOnClickListener(this);
+
+        //MAPA
+        mapaIris = findViewById(R.id.mapaIris);
+        mapaIris.setOnClickListener(this);
+
+        mapaAugusta = findViewById(R.id.mapaAugusta);
+        mapaAugusta.setOnClickListener(this);
+
+        mapaCiutat = findViewById(R.id.mapaCiutat);
+        mapaCiutat.setOnClickListener(this);
+
+        mapaGranollers = findViewById(R.id.mapaGranollers);
+        mapaGranollers.setOnClickListener(this);
+
+        //MOSTRAR TARGETAS I SPINNER
         card1= findViewById(R.id.card1);
         card2=findViewById(R.id.card2);
         card3=findViewById(R.id.card3);
@@ -65,8 +78,9 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
 
     @Override
     public void onClick(View v) {
-        Integer posicio=null;
+        Integer posicio=null; ///LA POSICIO QUE INDICARA L'ARRAY
 
+        ///PER SABER LA WEB DEL HOTEL
         switch(v.getId()){
             case R.id.webIris: posicio=0;break;
             case R.id.webAugusta:posicio=1;break;
@@ -79,6 +93,7 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
             startActivity(intent);
         }
 
+        ///PER SABER A QUI TRUQUEM
         posicio=null;
         switch(v.getId()){
             case R.id.trucaIris: posicio=0;break;
@@ -91,6 +106,21 @@ public class hotels extends AppCompatActivity implements View.OnClickListener, A
             Uri tel = Uri.parse("tel:"+contacte[posicio]);
             Intent intent2 = new Intent(Intent.ACTION_DIAL,tel);
             startActivity(intent2);
+        }
+
+        ///PER SABER EL MAPA
+        posicio=null;
+        switch(v.getId()){
+            case R.id.mapaIris: posicio=0;break;
+            case R.id.mapaAugusta: posicio=1;break;
+            case R.id.mapaCiutat: posicio=2;break;
+            case R.id.mapaGranollers: posicio=3;break;
+        }
+
+        if(posicio!=null){
+            Uri mapa = Uri.parse("geo:"+mapaHotel[posicio]);
+            Intent intent3 = new Intent(Intent.ACTION_VIEW,mapa);
+            startActivity(intent3);
         }
     }
 
