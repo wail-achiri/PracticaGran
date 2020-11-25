09cfce255d19e145d221d9a1b9faf771cf72a8e7
1 <mzafram2001@gmail.com>
2020-11-25 10:19:57 +0100

    - Cambio diseño de Restaurants y Parkings. - Implementado la flecha para volver al mainactivity. - Implementado emojis para filtrar mejor los Restaurants.

--- app/src/main/java/com/example/practicagran/Parkings.java
+++ app/src/main/java/com/example/practicagran/Parkings.java
@@ -2,13 +2,41 @@ package com.example.practicagran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 
-public class Parkings extends AppCompatActivity {
+public class Parkings extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
 
+    ImageView flecha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parkings);
+
+
+        flecha = findViewById(R.id.back);
+        flecha.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        /// RETURN MAIN ACTIVITY
+        if(v.getId()==R.id.back){
+            Intent inici = new Intent(this,MainActivity.class);
+            startActivity(inici);
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/practicagran/Restaurants.java
+++ app/src/main/java/com/example/practicagran/Restaurants.java
@@ -8,18 +8,20 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Restaurants extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     String[] urls = {"https://www.eltrabuc.com/", "https://www.tripadvisor.es/Restaurant_Review-g670666-d10820059-Reviews-El_Gato_Verde-Granollers_Catalonia.html", "https://elcellerdejabugo.com/", "http://www.restaurantcuynes.com/es/restaurante-cuynes-granollers_134455.html", "http://www.fondaeuropa.eu/", "https://www.tripadvisor.es/Restaurant_Review-g670666-d8779106-Reviews-L_Anima_De_Granollers-Granollers_Catalonia.html","http://www.vinyam.es/"};
     String[] contacte = {"938 70 86 57", "938 40 38 36", "938 70 96 00", "938 61 30 37", "938 70 03 12", "938 70 42 91","930 23 28 99"};
-    String[] mapita = {"41.5908641,2.284703", "41.6107729,2.2869426", "41.608867,2.289158", "41.6088871,2.2899162", "41.6082916,2.2890501", "41.61756,2.2952368,14", "41.6069444,2.2841667"};
+    String[] mapita = {"41.590960368842374, 2.2847244553087127", "41.61092529067517, 2.2868889534571166", "41.60946061790583, 2.2892438306882825", "41.60899938572303, 2.289948384145327", "41.60841190871738, 2.2890715553092766", "41.60882475179065, 2.2906984957849708", "41.60712888819498, 2.284177426473188"};
     TextView webTrabuc,webGato,webJabugo,webCuynes,webFondaEur,webAnima,webVinyam;
     TextView trucaTrabuc,trucaGato,trucaJabugo,trucaCuynes,trucaFondaEur,trucaAnima,trucaVinyam;
     TextView mapaTrabuc,mapaGato,mapaJabugo,mapaCuynes,mapaFondaEur,mapaAnima,mapaVinyam;
     Spinner restaurants_spinner;
     CardView card1, card2, card3, card4, card5, card6, card7;
+    ImageView flecha;
 
 
     @Override
@@ -109,12 +111,23 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
 
         restaurants_spinner = findViewById(R.id.spinner_restaurants);
         restaurants_spinner.setOnItemSelectedListener(this);
+
+        // FLECHA
+        flecha = findViewById(R.id.back);
+        flecha.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         Integer posicio=null;
 
+        /// RETURN MAIN ACTIVITY
+        if(v.getId()==R.id.back){
+            Intent inici = new Intent(this,MainActivity.class);
+            startActivity(inici);
+        }
+
+        /// PÀGINES WEB
         switch(v.getId()){
             case R.id.webTrabuc: posicio=0;break;
             case R.id.webGatoVerda:posicio=1;break;
@@ -130,6 +143,7 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
             startActivity(intent);
         }
 
+        /// TLF
         posicio=null;
         switch(v.getId()){
             case R.id.trucaTrabuc: posicio=0;break;
@@ -147,6 +161,7 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
             startActivity(intent2);
         }
 
+        /// GOOGLE MAPS
         posicio=null;
         switch(v.getId()){
             case R.id.mapaTrabuc: posicio=0;break;
@@ -167,6 +182,8 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        /// SPINNER
         if(position==0){
             card1.setVisibility(View.VISIBLE);
             card2.setVisibility(View.VISIBLE);
@@ -193,7 +210,7 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
             card7.setVisibility(View.GONE);
         }else if(position==3){
             card1.setVisibility(View.VISIBLE);
-            card2.setVisibility(View.VISIBLE);
+            card2.setVisibility(View.GONE);
             card3.setVisibility(View.GONE);
             card4.setVisibility(View.VISIBLE);
             card5.setVisibility(View.VISIBLE);
@@ -204,7 +221,7 @@ public class Restaurants extends AppCompatActivity implements View.OnClickListen
             card1.setVisibility(View.GONE);
             card2.setVisibility(View.GONE);
             card3.setVisibility(View.VISIBLE);
-            card4.setVisibility(View.VISIBLE);
+            card4.setVisibility(View.GONE);
             card5.setVisibility(View.GONE);
             card6.setVisibility(View.VISIBLE);
             card7.setVisibility(View.GONE);
